d3e88fec5e052f081fc73535aa8d79de5a587300
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-04-14 12:41:34 +0000

    Implement XML reader

---
+++ src/main/java/sub/fwb/XmlReader.java
@@ -0,0 +1,64 @@
+package sub.fwb;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+public class XmlReader {
+
+	public void addElementsToSet(InputStream xmlFile, Set<String> elements) {
+		XMLInputFactory factory = XMLInputFactory.newInstance();
+
+		try {
+			XMLEventReader eventReader = factory.createXMLEventReader(xmlFile);
+
+			while (eventReader.hasNext()) {
+				XMLEvent event = eventReader.nextEvent();
+
+				switch (event.getEventType()) {
+
+				case XMLStreamConstants.START_ELEMENT:
+					StartElement startTag = event.asStartElement(); 
+					String elementName = startTag.getName().getLocalPart();
+					
+					List<String> attributes = new ArrayList<String>();
+					String allAttributesString = "";
+					
+					Iterator<Attribute> iter = startTag.getAttributes();
+					while (iter.hasNext()) {
+						Attribute attr = iter.next();
+						attributes.add(attr.getName().getLocalPart());
+					}
+					Collections.sort(attributes);
+					
+					for (String a : attributes) {
+						allAttributesString += " " + a;
+					}
+					
+					elements.add("<" + elementName + ">" + allAttributesString);
+					
+					break;
+				default:
+					// ignore all the other events
+				}
+			}
+
+			eventReader.close();
+
+		} catch (XMLStreamException e) {
+			throw new IllegalArgumentException("Error reading XML", e);
+		}
+
+	}
+}
--- src/test/java/sub/fwb/XmlReaderTest.java
+++ src/test/java/sub/fwb/XmlReaderTest.java
@@ -2,6 +2,13 @@ package sub.fwb;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +24,21 @@ public class XmlReaderTest {
 	}
 
 	@Test
-	public void test() {
+	public void test() throws Exception {
+		XmlReader reader = new XmlReader();
+		Set<String> elements = new TreeSet<String>();
+		InputStream xml = readFile("tei1.xml");
 		
+		reader.addElementsToSet(xml, elements);
+		
+		for (String s : elements) {
+			System.out.println(s);
+		}
+	}
+	
+	private InputStream readFile(String file) throws FileNotFoundException {
+		File dir = new File(System.getProperty("user.dir") + "/src/test/resources/files");
+		return new FileInputStream(new File(dir, file));
 	}
 
 }
