8e8f38ed9d6f2b9d0ec75407251660937d432c1d
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-04-14 11:01:29 +0000

    Add some empty classes

---
+++ src/main/java/sub/fwb/Main.java
@@ -0,0 +1,10 @@
+package sub.fwb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/test/java/sub/fwb/XmlReaderTest.java
@@ -0,0 +1,24 @@
+package sub.fwb;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class XmlReaderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
